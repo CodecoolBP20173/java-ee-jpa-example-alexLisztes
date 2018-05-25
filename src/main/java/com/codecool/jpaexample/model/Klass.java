b6cf62521c93f29c06c847d1b9b0b845dce0749d
Alex <lisztes.alex@gmail.com>
2018-05-25 11:23:48 +0200

    Excercise 8

--- src/main/java/com/codecool/jpaexample/model/Klass.java
+++ src/main/java/com/codecool/jpaexample/model/Klass.java
@@ -4,8 +4,12 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "Class")
 public class Klass {
+    @Id
     private String name;
+    @OneToMany
     private Set<Student> students = new HashSet<>();
 
     public Klass() {}
